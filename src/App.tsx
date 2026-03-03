2c6cf9bef282a775a43a03c7fa669f930035e8d8
anamariata <u20211e348@upc.edu.pe>
2026-03-03 13:10:38 -0500

    publicacion de la pagina

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "@/components/auth/AuthProvider";
 import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
 import { LayoutDoctor } from "@/components/layout/LayoutDoctor";
@@ -36,7 +36,7 @@ const App = () => (
         <AuthProvider>
           <Routes>
             {/* Rutas públicas */}
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<Navigate to="/app/doctor/inicio" replace />} />
             <Route path="/login-doctor" element={<LoginDoctor />} />
             <Route path="/login-admin" element={<LoginAdmin />} />
 
